4b6710951855e390e5fc67e68a6d65991201b12a
Chris Chan <cchan9528@gmail.com>
2017-06-05 15:05:53 -0700

    Abstracted parameters out and refactored.

--- visualization/occlusion.py
+++ visualization/occlusion.py
@@ -15,7 +15,12 @@
 #               classifier/model originally used
 #               (thus salience is relative)
 
-# heavily inspired by Ryan Compton, Clariai
+# dependencies: Pillow (or PIL)
+#               numpy
+#               matplotlib
+
+
+# heavily inspired by Ryan Compton, Clarifai
 # http://blog.clarifai.com/what-convolutional-neural-networks-see-at-when-they-see-nudity/#.WSZ_VBiZPMV
 
 ################################################################
@@ -23,14 +28,17 @@
 #                         config                               #
 #                                                              #
 ################################################################
-from Pillow import Image, ImageDraw
+from PIL import Image, ImageDraw
 import numpy
-import matplotplib.pyplot as matplot
+import matplotlib.pyplot as matplot
 
-# constants
-srcalpha = 1000;
-occlen   = 64;
-stride   = 48;      # small => many calls; big => less precision
+# parameters
+srcalpha     = 1000;    # rgba a value for src image
+blendalph    = .8;      # alpha parameter for blending
+occlen       = 64;      # length of side of square occlusion
+stride       = 48;      # small => many calls; big => less precision;
+heatmapScale = 80;      # scales salience map matrix values before overlaying
+filepath     = "data/sample/10_left.jpeg"; # relative to this script's dir
 
 ################################################################
 #                                                              #
@@ -41,20 +49,22 @@ stride   = 48;      # small => many calls; big => less precision
 #######################################
 # obtain the score for occluded image
 #######################################
-def score(occludedImage):
-    return # model output here
+def getScore(occludedImage):
+    return 1;# model output here
 
 #######################################
 # obtain occlusions for source image
 #######################################
 def getOcclusions(src, occlen, stride):
-    # square occlusions
+    # square occlusions; p1 = top-left corner, p2 = bottom-right corner
     w = occlen;
     h = occlen;
     occlusions = [];
-    for top in range(0, src.size[1], stride):
-        for left in range(0, src.size[0], stride):
-            occlusions.append( (left, top, left+width, top+height) );
+    for p1_y in range(0, src.size[1], stride):
+        for p1_x in range(0, src.size[0], stride):
+            p2_x = p1_x + w; p2_y = p1_y + h;
+            occlusions.append( (p1_x, p1_y, p2_x, p2_y) );
+    print("generating occlusions for source...");
     return occlusions;
 
 #######################################
@@ -64,47 +74,52 @@ def occlude(src, occlusion):
     occluded = src.copy();
     black = ImageDraw.Draw(occluded);
     black.rectangle(occlusion, fill=True);
+    print("occluding source image with ", occlusion);
     return occluded;
 
 #######################################
 # blend salience map with source image
 #######################################
-def overlaySalienceMap(src, salienceMap):
+def overlayHeatmap(src, heatmap):
     colormap = matplot.get_cmap('jet');
-    coloredSalience = Image.fromarray(numpy.uint8(colormap(salienceMap)*255));
-    return Image.blend(src, coloredSalience, .8);
+    coloredSalience = Image.fromarray(numpy.uint8(colormap(heatmap)*255));
+    print("overlaying salience heatmap...")
+    return Image.blend(src, coloredSalience, blendalpha);
 
 #######################################
 # produce occlusion-based salience map
 #######################################
-def getSalienceMap(src, occArea, stride):
-    # create occlusions; specified by top-left, bottom-right pts
-    occlusions = getOcclusions(src, occArea, stride);
+def getSalienceMap(src, occlen, stride):
+    # create occlusions; specified by top-left, bottom-right corners
+    occlusions = getOcclusions(src, occlen, stride);
 
     # create occluded copies of src image
     occ_scores = [];
     for occlusion in occlusions:
         occludedImage = occlude(src, occlusion);
-        occ_scores.append( (occlusion, score(occludedImage)) );
+        occ_score = (occlusion, getScore(occludedImage));
+        occ_scores.append( occ_score);
+        print(occ_score);
 
     # create salience heatmap
-    salienceMap = numpy.zeros(src.size);
+    heatmap = numpy.zeros(src.size);
     for i in range(len(occ_scores)):
+        print("modifying heatmap with ",  (occ_scores[i]));
         (occlusion, score) = occ_scores[i];
         mask = numpy.zeros(src.size);
         mask[occlusion[0]:occlusion[2], occlusion[1]:occlusion[3]] = score;
-        salienceMap = salienceMap + (mask - salienceMap)/(i+1);
-    salienceMap = numpy.transpose(salienceMap) * 80;
+        heatmap = heatmap + (mask - heatmap)/(i+1);
+    heatmap = numpy.transpose(heatmap) * heatmapScale;
 
     # return blended result
-    return overlaySalienceMap(src, salienceMap);
+    return overlayHeatmap(src, heatmap);
 
 ################################################################
 #                                                              #
 #                        runner                                #
 #                                                              #
 ################################################################
-if __name__ = "__main__":
-    src = Image.open("data/sample/10_left.jpeg"); src.putalpha(srcalpha);
+if __name__ == "__main__":
+    src = Image.open(filepath); src.putalpha(srcalpha);
     salienceMap = getSalienceMap(src, occlen, stride);
     salienceMap.show();
