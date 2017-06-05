c7a251d601a38a23303af2841347e5a28910ed9e
Chris Chan <cchan9528@gmail.com>
2017-06-05 02:25:22 -0700

    started occlusion-based salience maps

---
+++ visualization/README.txt
@@ -0,0 +1,18 @@
+Activate the virtual environment first!...
+    $ source myvenv/bin/activate
+
+...Deactivate the virtual environment when you leave!
+    $ deactivate
+
+
+Note: I used pip to install the SciPy core stack:
+	- numpy
+	- matplotlib
+	- scipy
+	- pandas
+	- sympy
+	- ipython
+	- nose
+	- jupyter
+
+    this was more for future experimentation.
---
+++ visualization/occlusion.py
@@ -0,0 +1,36 @@
+################################################################
+#                                                              #
+#                         doc                                  #
+#                                                              #
+################################################################
+
+# overview    : obtain heatmap by looking at
+#               salience patches; this is done
+#               by occluding the image many times
+#               and creating a numeric overlay based
+#               on scores obtained for occluded copies
+
+# note        : many copies of the input image are made;
+#               each is scored according to the
+#               classifier/model originally used
+#               (thus salience is relative)
+
+################################################################
+#                                                              #
+#                         config                               #
+#                                                              #
+################################################################
+
+
+################################################################
+#                                                              #
+#                        helpers                               #
+#                                                              #
+################################################################
+
+
+################################################################
+#                                                              #
+#                        runners                               #
+#                                                              #
+################################################################
